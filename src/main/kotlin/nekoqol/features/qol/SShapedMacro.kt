4510c4ad33e2878248db3cbdbb6ae0f150bdb5e6
Der_s <isthatder@gmail.com>
2022-04-02 23:39:30 -0600

    Fix shit bro2

--- src/main/kotlin/nekoqol/features/qol/SShapedMacro.kt
+++ src/main/kotlin/nekoqol/features/qol/SShapedMacro.kt
@@ -86,13 +86,13 @@ class SShapedMacro {
 
         when(dir) {
             EnumFacing.WEST -> {
-                if (mc.theWorld.getBlockState(pos.immutable.south(1)).block != Blocks.air) {
+                if (mc.theWorld.getBlockState(pos.immutable.south(1)).block != Blocks.air || mc.theWorld.getBlockState(pos).block != Blocks.air) {
                     //left
                     timer(1000) {
                         KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
                         KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
                     }
-                } else if (mc.theWorld.getBlockState(pos.immutable.north(1)).block != Blocks.air) {
+                } else if (mc.theWorld.getBlockState(pos.immutable.north(1)).block != Blocks.air || mc.theWorld.getBlockState(pos).block != Blocks.air) {
                     //right
                     timer(1000) {
                         KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
