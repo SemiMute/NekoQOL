a5713c6e364db59743985b87844e50b4c4537191
Der_s <isthatder@gmail.com>
2022-04-02 22:41:17 -0600

    Fix shit bro

--- src/main/kotlin/nekoqol/features/qol/SShapedMacro.kt
+++ src/main/kotlin/nekoqol/features/qol/SShapedMacro.kt
@@ -12,254 +12,174 @@ import nekoqol.utils.Utils.isInLimbo
 import nekoqol.utils.Utils.isInLobby
 import nekoqol.utils.Utils.isPrivateIsland
 import nekoqol.utils.Utils.modMessage
+import net.minecraft.block.Block
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.inventory.GuiInventory
 import net.minecraft.client.settings.KeyBinding
+import net.minecraft.init.Blocks
+import net.minecraft.util.EnumFacing
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.InputEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent
+import scala.annotation.switch
 import java.util.*
+import kotlin.concurrent.timer
 import kotlin.concurrent.timerTask
 import kotlin.math.roundToInt
 import kotlin.random.Random
 
 var isActive = false
 var failSafeActive = false;
-var autoReconnect = false
-var shouldLeftClick = false
 
-private var lastY = 0.0
-private var getBlock = 0.0
-var getBlockZ = 0f
-var getBlockX = 0f
-var lastTurnAround = System.currentTimeMillis()
-var yDecrease = false
-var sendWebhook = false
-var lastdir = 1
+var thread: Thread? = null
+var lastUpdate = 0L
 
-class SShapedMacro {
-    private var thread: Thread? = null
-    private var lastUpdate: Long = 0
-    private var sShapedThread: Thread? = null
-    private var sLastUpdate: Long = 0
+var onWorldCooldown: Long = 0
 
+fun startMacro() {
+    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
+    isActive = true
+}
 
-    fun startMacro() {
-        isActive = true
-        failSafeActive = true
-        KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
-        if(lastdir == 1){
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
-        } else {
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
-        }
-        updateY()
-    }
+fun stopMacro() {
+    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, false)
+    isActive = false
+}
 
-    fun updateY() {
-        Timer().schedule(timerTask {
-            lastY = mc.thePlayer.posY
-            if(isActive){
-                updateY()
-            }
-        }, 1000)
-    }
-    @SubscribeEvent
-    fun onKeyPress(event: InputEvent.KeyInputEvent) {
-        if (keyBinds[0].isPressed) {
-            if (isActive || failSafeActive) {
-                isActive = false
-                failSafeActive = false
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.keyCode, false)
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, false)
-                modMessage("&bS Shaped Macro&f has been toggled &c&lOFF&f!")
-                yDecrease = false
-                sendWebhook = false
-            } else {
-                if(!isPrivateIsland()){
-                    return UChat.chat("&dFrom ${nameArray[Random.nextInt(nameArray.size)]} &7What, you want to farm some air? Go to your (&7⏣ &aPrivate Island&7) to start the &bS Shaped Macro&7...")
+@SubscribeEvent
+fun onMove(event: WorldTickEvent) {
+    if(mc.thePlayer.lastTickPosX == mc.thePlayer.posX) return
+    if(mc.thePlayer.lastTickPosY == mc.thePlayer.posY) return
+    if(mc.thePlayer.lastTickPosZ == mc.thePlayer.posZ) return
+
+    checkBlocks()
+}
+
+fun checkBlocks() {
+    val dir = mc.thePlayer.horizontalFacing
+    val pos = mc.thePlayer.position
+
+    when(dir) {
+        EnumFacing.NORTH -> {
+            var pos2 = pos.add(-1, 0, 0)
+
+            if(mc.theWorld.getBlockState(pos2).block != Blocks.air){
+                //left
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
                 }
-                isActive = true;
-                failSafeActive = true
-                //KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.keyCode, true)
-                modMessage("&bS Shaped Macro &fhas been toggled &a&lON&f!")
-                startMacro()
-                lastdir = 1
-                if(NekoQOL.nekoconfig.discordURL == "" && NekoQOL.nekoconfig.discordPost){
-                    modMessage("&cERROR: It seems you are wanting to get discord notifications, but dont have a Webhook setup! Please correct this and try again...")
-                    isActive = false
-                    failSafeActive = false
-                    modMessage("&bS Shaped Macro&f has been force toggled &c&lOFF&f due to an &cERROR&f occurring")
-                    return
+            } else if(mc.theWorld.getBlockState(pos2.add(2,0,0)).block.isCollidable) {
+                //right
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
                 }
-                //KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
             }
         }
-    }
-    var onWorldCooldown: Long = 0
+        EnumFacing.SOUTH -> {
+            var pos2 = pos.add(1, 0, 0)
 
-    @SubscribeEvent
-    fun onWorldCheck(event: WorldEvent.Load){
-        if(isActive){
-            isActive = false
-            modMessage("&bS Shaped Macro&f has been force toggled &c&lOFF&f due to a world change")
-            if(NekoQOL.nekoconfig.discordPost){
-                DiscordWebhook(NekoQOL.nekoconfig.discordURL).setContent(getDiscordPing("Detected a world change. Disabling Macro while failsafes activate")).execute()
+            if(mc.theWorld.getBlockState(pos2).block != Blocks.air){
+                //left
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
+                }
+            } else if(mc.theWorld.getBlockState(pos2.add(-2,0,0)) != Blocks.air) {
+                //right
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
+                }
             }
         }
-        onWorldCooldown = System.currentTimeMillis()
+        EnumFacing.EAST ->  {
+            var pos2 = pos.add(0, 0, -1)
+
+            if(mc.theWorld.getBlockState(pos2).block != Blocks.air){
+                //left
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
+                }
+            } else if(mc.theWorld.getBlockState(pos2.add(0,0,2)).block != Blocks.air) {
+                //right
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
+                }
+            }
+        }
+        EnumFacing.WEST ->  {
+            var pos2 = pos.add(0, 0, 1)
+
+            if(mc.theWorld.getBlockState(pos2).block != Blocks.air){
+                //left
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
+                }
+            } else if(mc.theWorld.getBlockState(pos2.add(0,0,-2)).block != Blocks.air) {
+                //right
+                timer(1000) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
+                }
+            }
+        }
     }
-    @SubscribeEvent
-    fun onTick(event: TickEvent.ClientTickEvent) {
-        if (event.phase != TickEvent.Phase.START || failSafeActive && isPrivateIsland() && onWorldCooldown + 1500 <= System.currentTimeMillis()) return
-        if (thread?.isAlive == true || lastUpdate + 2500 > System.currentTimeMillis()) return
-        thread = Thread({
-            if(failSafeActive){
-                if(isInLobby()){
-                    modMessage("&cFAILSAFE: &fPlayer is in &7⏣ &6Hypixel Lobby&f...\n&7Attempting to correct players' position...")
-                    mc.thePlayer.sendChatMessage("/play skyblock")
-                }
-                if(isInHub()){
-                    modMessage("&cFAILSAFE: &fPlayer is in the &7⏣ &bSkyblock Village&f..\n&7Attempting to correct players' position...")
-                    mc.thePlayer.sendChatMessage("/warp home")
-                }
-                if(isInLimbo()){
-                    modMessage("&cFAILSAFE: &fPlayer is in the &7⏣ &cLimbo&f..\n&7Attempting to correct players' position...")
-                    mc.thePlayer.sendChatMessage("/lobby")
-                }
-                Timer().schedule(timerTask {
-                    if(isPrivateIsland()){
-                        if(isActive) {
-                            return@timerTask
-                        }
-                        modMessage("&cFAILSAFE: &fPlayer is in &7⏣ &aPrivate Island&f\n&7Attempting to start up S Shaped Macro")
-                        DiscordWebhook(NekoQOL.nekoconfig.discordURL).setContent(getDiscordPing("Starting up **S Shaped** due to a player location correction")).execute()
-                        startMacro()
+}
+
+fun timer(delay: Int, callback: () -> Unit) {
+    Timer().schedule(timerTask {
+        callback()
+    }, delay.toLong())
+}
+
+@SubscribeEvent
+fun onWorldCheck(event: WorldEvent.Load){
+    if(isActive){
+        stopMacro()
+        modMessage("&bS Shaped Macro&f has been force toggled &c&lOFF&f due to a world change")
+        if(NekoQOL.nekoconfig.discordPost){
+            DiscordWebhook(NekoQOL.nekoconfig.discordURL).setContent(getDiscordPing("Detected a world change. Disabling Macro while failsafes activate")).execute()
+        }
+    }
+    onWorldCooldown = System.currentTimeMillis()
+}
+@SubscribeEvent
+fun onTick(event: TickEvent.ClientTickEvent) {
+    if (event.phase != TickEvent.Phase.START || failSafeActive && isPrivateIsland() && onWorldCooldown + 1500 <= System.currentTimeMillis()) return
+    if (thread?.isAlive == true || lastUpdate + 2500 > System.currentTimeMillis()) return
+    thread = Thread({
+        if(failSafeActive){
+            if(isInLobby()){
+                modMessage("&cFAILSAFE: &fPlayer is in &7⏣ &6Hypixel Lobby&f...\n&7Attempting to correct players' position...")
+                mc.thePlayer.sendChatMessage("/play skyblock")
+            }
+            if(isInHub()){
+                modMessage("&cFAILSAFE: &fPlayer is in the &7⏣ &bSkyblock Village&f..\n&7Attempting to correct players' position...")
+                mc.thePlayer.sendChatMessage("/warp home")
+            }
+            if(isInLimbo()){
+                modMessage("&cFAILSAFE: &fPlayer is in the &7⏣ &cLimbo&f..\n&7Attempting to correct players' position...")
+                mc.thePlayer.sendChatMessage("/lobby")
+            }
+            Timer().schedule(timerTask {
+                if(isPrivateIsland()){
+                    if(isActive) {
+                        return@timerTask
                     }
-                }, 10000)
-            }
-            lastUpdate = System.currentTimeMillis()
-        }, "S Shaped Failsafe")
-        thread!!.start()
-    }
-
-    @SubscribeEvent
-    fun onMacro(event: TickEvent.PlayerTickEvent){
-        getBlock = mc.thePlayer.posY
-
-        getBlockZ = mc.thePlayer.posZ.roundToInt().toFloat()
-        getBlockX = mc.thePlayer.posX.roundToInt().toFloat()
-        if(Minecraft.getMinecraft().currentScreen is GuiInventory && isActive && failSafeActive){
-            isActive = false
-            failSafeActive = false
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.keyCode, false)
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, false)
-            modMessage("&bS Shaped Macro&f has been toggled &c&lOFF&f due to a GUI being opened")
+                    modMessage("&cFAILSAFE: &fPlayer is in &7⏣ &aPrivate Island&f\n&7Attempting to start up S Shaped Macro")
+                    DiscordWebhook(NekoQOL.nekoconfig.discordURL).setContent(getDiscordPing("Starting up **S Shaped** due to a player location correction")).execute()
+                    startMacro()
+                }
+            }, 10000)
         }
-        if(isActive){
-            mc.thePlayer.rotationPitch = NekoQOL.nekoconfig.sShapedPitch
-            if(NekoQOL.nekoconfig.sShapedYaw == 0){
-                mc.thePlayer.rotationYaw = 180F
-            } else if(NekoQOL.nekoconfig.sShapedYaw == 1){
-                mc.thePlayer.rotationYaw = 0F
-            }else if(NekoQOL.nekoconfig.sShapedYaw == 2){
-                mc.thePlayer.rotationYaw = -90F
-            }else if(NekoQOL.nekoconfig.sShapedYaw == 3){
-                mc.thePlayer.rotationYaw = 90F
-            }
-            if(isActive) {
-                if(lastY !== getBlock){
-                    if(System.currentTimeMillis() - lastTurnAround > 1000){
-                        lastTurnAround = System.currentTimeMillis()
-                        if(mc.gameSettings.keyBindLeft.isKeyDown){
-                            if(!yDecrease){
-                                yDecrease = true
-                                Timer().schedule(timerTask {
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
-                                    modMessage("&cActivated RIGHT DIRECTION")
-                                    mc.thePlayer.sendChatMessage("/sethome")
-                                    yDecrease = false
-                                    lastdir = 1
-                                }, 1000)
-                            }
-                        } else if(mc.gameSettings.keyBindRight.isKeyDown){
-                            if(!yDecrease){
-                                yDecrease = true
-                                Timer().schedule(timerTask {
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
-                                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
-                                    modMessage("&cActivated LEFT DIRECTION")
-                                    mc.thePlayer.sendChatMessage("/sethome")
-                                    yDecrease = false
-                                    lastdir = 2
-                                }, 1000)
-                            }
-                        }
-                    }
-                }
-                if(System.currentTimeMillis() - lastTurnAround > 5000){
-                    lastTurnAround = System.currentTimeMillis()
-                    if(mc.gameSettings.keyBindLeft.isKeyDown){
-                        if(!yDecrease){
-                            yDecrease = true
-                            Timer().schedule(timerTask {
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, false)
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, true)
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
-                                modMessage("&cActivated RIGHT DIRECTION OTHER")
-                                mc.thePlayer.sendChatMessage("/sethome")
-                                yDecrease = false
-                            }, 250)
-                            lastdir = 1
-                        }
-                    } else if(mc.gameSettings.keyBindRight.isKeyDown){
-                        if(!yDecrease){
-                            yDecrease = true
-                            Timer().schedule(timerTask {
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.keyCode, false)
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.keyCode, true)
-                                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.keyCode, true)
-                                modMessage("&cActivated LEFT DIRECTION OTHER")
-                                mc.thePlayer.sendChatMessage("/sethome")
-                                yDecrease = false
-                            }, 250)
-                            lastdir = 2
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @SubscribeEvent
-    fun onWorldLoad(event: WorldEvent.Load){
-        Timer().schedule(timerTask {
-            val world = event.world
-
-            if(world !== mc.theWorld){
-                return@timerTask
-            }
-            if(failSafeActive){
-                if(isInLimbo()){
-                    UChat.chat("Your dumbass is in limbo. nice going mf")
-                }
-                if(isInLobby()){
-                    UChat.chat("&cWow, congrats your in the lobby...")
-                }
-                if(isInHub()){
-                    UChat.chat("&cWow, congrats your in the fucking skyblock hub you dumb shit...")
-                }
-            }
-        }, 5 * 20 * 60)
-    }
+        lastUpdate = System.currentTimeMillis()
+    }, "S Shaped Failsafe")
+    thread!!.start()
 }
